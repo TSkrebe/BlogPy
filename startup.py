642c6e4eab76acc0f743c3d1f01214addfb57bf6
Titas Skrebe <titas.skrebe@gmail.com>
2016-02-01 13:05:49 +0000

    update gitignore

--- startup.py
+++ startup.py
@@ -1,7 +1,22 @@
 import os
 
-from app import create_app
+from app import db
+from app.models import User
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager, Shell
+
+application = os.environ.get('FLASK_CONFIG') or 'testing'
+
+migrate = Migrate(application, db)
+manager = Manager(application)
+manager.add_command('db', MigrateCommand)
+
+
+def make_shell_context():
+    return dict(app=application, User=User, db=db)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
-    create_app('testing').run()
+    manager.run()
 
